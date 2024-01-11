3dd0f5bf4a52446047d3775a23e98411f14cacdf
Ali Ozen <aliozendev@gmail.com>
2024-01-11 16:17:10 +0100

    zsh plugins are merged into one file

--- install.sh
+++ install.sh
@@ -39,23 +39,9 @@ echo_title "OH MY ZSH"
 chmod +x "$DOTFILES_PATH"/installations/oh-my-zsh.sh
 "$DOTFILES_PATH/installations/oh-my-zsh.sh"
 
-# Clone zsh-autosuggestions plugin
-echo_title "ZSH AUTO SUGGESTION"
-if ! [[ -d "$DOTFILES_PATH/plugins/zsh-autosuggestions" ]]; then
-    git clone --quiet https://github.com/zsh-users/zsh-autosuggestions "$DOTFILES_PATH/plugins/zsh-autosuggestions"
-    echo -e "${SUCCESS_COLOR}zsh-autosuggestions plugin cloned successfully${NO_COLOR}"
-else
-    echo -e "${WARNING_COLOR}zsh-autosuggestions is already installed${NO_COLOR}"
-fi
-
-# Clone zsh-syntax-highlighting plugin
-echo_title "ZSH SYNTAX HIGHLIGHTING"
-if ! [[ -d "$DOTFILES_PATH/plugins/zsh-syntax-highlighting" ]]; then
-    git clone --quiet https://github.com/zsh-users/zsh-syntax-highlighting.git "$DOTFILES_PATH/plugins/zsh-syntax-highlighting"
-    echo -e "${SUCCESS_COLOR}zsh-syntax-highlighting plugin cloned successfully${NO_COLOR}"
-else
-    echo -e "${WARNING_COLOR}zsh-syntax-highlighting is already installed${NO_COLOR}"
-fi
+# Zsh Plugins
+chmod +x "$DOTFILES_PATH"/installations/zsh-plugins.sh
+"$DOTFILES_PATH/installations/zsh-plugins.sh"
 
 # Override .zshrc
 echo_title ".ZSHRC"
