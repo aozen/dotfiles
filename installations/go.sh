e249960706e3a81989c25be38d98c324353fd985
Ali Ozen <aliozendev@gmail.com>
2024-01-12 09:39:29 +0100

    added empty lines

--- installations/go.sh
+++ installations/go.sh
@@ -2,7 +2,6 @@
 source ./.env
 source ./helper.sh
 
-# Check if Go is already installed
 if command -v go &> /dev/null; then
     echo -e "${WARNING_COLOR}Go is already installed${NO_COLOR}"
     echo "$(go version)"
@@ -11,13 +10,15 @@ fi
 
 echo "Downloading Go..."
 cd /tmp/
+
 wget -q https://golang.org/dl/go1.21.5.linux-amd64.tar.gz && echo "Go downloaded"
+
 sudo tar -C /usr/local -xzf go1.21.5.linux-amd64.tar.gz > /dev/null 2>&1 && echo "Go installed"
+
 mkdir -p $HOME/go/bin $HOME/go/pkg $HOME/go/src
 
 cd - > /dev/null
 
-# Check if Go installation is successful
 if command -v go &> /dev/null; then
     echo -e "${SUCCESS_COLOR}Go installed successfully${NO_COLOR}"
     echo "$(go version)"
